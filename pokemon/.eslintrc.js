27a2e3bb60abb12257d011811265d326a633de1d
Qwaszxman <qwaszxman@gmail.com>
2020-03-01 03:47:35 +0000

    update

--- pokemon/.eslintrc.js
+++ pokemon/.eslintrc.js
@@ -22,6 +22,15 @@ module.exports = {
   plugins: [
     'react'
   ],
-  rules: {
-  }
+  "rules": {
+    "no-const-assign": "warn",
+    "no-this-before-super": "warn",
+    "no-undef": "warn",
+    "no-unreachable": "warn",
+    "no-unused-vars": "warn",
+    "constructor-super": "warn",
+    "valid-typeof": "warn",
+    "no-extra-semi":"error",
+    
+}
 }
